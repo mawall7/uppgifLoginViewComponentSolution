12ee01cafef00f535e9a4848fe372921099786cd
Mattias Wallerman <wallermanmattias@yahoo.se>
2021-04-19 10:31:25 +0200

    ViewModel

---
+++ uppgifLoginViewComponent/ViewComponents/LoginViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uppgifLoginViewComponent.ViewComponents
+{
+    public class LoginViewComponent:ViewComponent
+    {
+       
+        public LoginViewModel Model { get; set; }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            try
+            {
+               // Model = new LoginViewModel { IsLoggedIn = User.Identity.IsAuthenticated, Name = User.Identity.Name };
+
+            }
+            catch (ArgumentNullException) { }
+
+            return View(Model);
+
+        }
+    }
+}
