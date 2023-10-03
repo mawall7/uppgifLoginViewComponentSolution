664efef38c2253cfb704996210b7c11ba4eee83f
Mattias Wallerman <wallermanmattias@yahoo.se>
2023-10-03 14:35:18 +0200

    -FeaturedCourses ViewComponent +css animation

---
+++ uppgifLoginViewComponent/Views/ViewComponents/FeauturedCoursesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using uppgifLoginViewComponent.Data;
+using uppgifLoginViewComponent.Models;
+
+namespace uppgifLoginViewComponent.Views.ViewComponents
+{
+    public class FeauturedCoursesViewComponent : ViewComponent
+    {
+        private SchoolContext _context { get; set; }
+        public FeauturedCoursesViewComponent(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var courses = await _context.Courses.ToListAsync();
+            return View(courses);
+            
+        }
+
+    }
+}
